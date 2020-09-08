57df4d6c381ca7675bb7cadf8ba1cd6a7741265d
alvin211 <alvinrisd@gmail.com>
2020-09-08 13:54:34 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Coba Gi Remote 2 / 3</title>
+  </head>
+  <body>
+
+    <h1>Hello World</h1>
+
+  </body>
+</html>
